3c931eb47405baa8805492977db7c91174a0ba28
Roshan Elvitigala <roshanrox911@gmail.com>
2020-10-15 12:11:59 +1100

    type array

---
+++ object.ts
@@ -0,0 +1,18 @@
+// const person: object = {
+//     name: 'Roshan',
+//     age: 28
+// };
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+//     name: 'Roshan',
+//     age: 28
+// };
+const person = {
+    name: 'Roshan',
+    age: 28
+};
+
+// console.log(person);
+console.log(person.name);
